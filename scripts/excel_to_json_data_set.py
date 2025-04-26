a65bb75c8efdfebcbea212870e998f40f0fe59ba
胡雷庭 <huleiting.hlt@bytedance.com>
2025-04-26 19:19:26 +0800

    merge data

--- scripts/excel_to_json_data_set.py
+++ scripts/excel_to_json_data_set.py
@@ -1,6 +1,8 @@
 import json
 import random
+import requests
 import pandas as pd
+from time import sleep
 
 
 file_path = '/Users/bytedance/Downloads/GiraffeTrainingData.xlsx'
@@ -122,8 +124,51 @@ def augment_dataset(sheet_name='WebUI Data With Thought', ratio=2):
 
 
 # 每 2、3 条取一条
-augment_dataset(ratio=2)
-augment_dataset(ratio=3)
+# augment_dataset(ratio=2)
+# augment_dataset(ratio=3)
+
+
+# 从 DB 拉取训练数据并合并到 thought 数据集中
+def prepare_similar_data(system_prompt):
+    """
+
+    """
+    result = []
+    base_url = 'https://nbq2u6x6.fn.bytedance.net/query_train_data'
+    for i in range(100):
+        url = base_url + f'?page={i+1}' + '&limit=100'
+        resp = requests.get(url).json()
+        resp_data = resp['data']
+        if resp_data:
+            for item in resp_data:
+                result.append({
+                    "instruction": system_prompt,
+                    "input": item['question'],
+                    "output": item['answer']
+                })
+        else:
+            break
+        sleep(1)
+    return result
+
+
+similar_data = prepare_similar_data('使用RobotFramework标准库和EasyUILib编写或重构WebUI自动化脚本')
+print(similar_data[0])
+
+# 合并 thought 数据集
+for one_file in ['giraffe_web_ui_with_thought.json', 'giraffe_web_ui_with_thought_en.json']:
+    with open('../data/' + one_file, encoding='utf-8') as f:
+        records = json.load(f)
+        similar_data.extend(records)
+    sleep(1)
+
+# 打乱顺序
+random.shuffle(similar_data)
+print(len(similar_data))
+print(similar_data[0])
+
+with open('../data/giraffe_web_ui_with_thought_all.json', 'w') as outfile:
+    json.dump(similar_data, outfile, ensure_ascii=False, indent=4)
 
 
 if __name__ == '__main__':
